c268a8f015e4ec1179590628d5d45ede9290db05
kouloudB <73066206+kouloudB@users.noreply.github.com>
2021-02-17 17:29:21 +0100

    Update Login.tsx

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -4,18 +4,19 @@ import { useHistory } from 'react-router-dom';
 function Login() {
   const [ cats, setCats] = useState('no url')
   const [ cat, setCat] = useState('nooo url')
+
   const history = useHistory();
-useEffect( ()=> {
-     (async function anyNameFunction() {
+  useEffect( ()=> {
+    (async function anyNameFunction() {
       const cache = await caches.open('accounts-cache');
       cache.add('/cats.json') 
       cache.put('/cats.json', new Response('{"james": "kitten", "daniel": "kitten"}'))
       const response = await cache.match('/cats.json');
       console.log("cache===>", response)
-      setCats(response? response?.toString() : 'fail')
+      setCats(response? response.toString() : 'fail')
       const respo = await cache.match('accounts-cache');
       console.log("cache===>", respo)
-      setCat(respo? respo?.toString() : 'fail respo')
+      setCat(respo? respo.toString() : 'fail respo')
     })();
    
   }, [])
@@ -27,6 +28,9 @@ useEffect( ()=> {
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
+      <p>{"cats "+cats}</p>
+      <p>{"cat=>>> "+cat}</p>
+
     </div>
   );
 }
