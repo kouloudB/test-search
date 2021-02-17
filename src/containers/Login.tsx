587e0a1ee37d1698fe5333fc497b3e5d482a3536
kouloudB <73066206+kouloudB@users.noreply.github.com>
2021-02-17 18:06:44 +0100

    Update Login.tsx

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -8,13 +8,13 @@ function Login() {
   const history = useHistory();
   useEffect( ()=> {
     (async function anyNameFunction() {
-      const cache = await caches.open('accounts-cache');
+      const cache = await caches.open('cats-cache');
       cache.add('/cats.json') 
       cache.put('/cats.json', new Response('{"james": "kitten", "daniel": "kitten"}'))
       const response = await cache.match('/cats.json');
       console.log("cache===>", response)
       setCats(response? response.toString() : 'fail')
-      const respo = await cache.match('accounts-cache');
+      const respo = await cache.match('cats-cache');
       console.log("cache===>", respo)
       setCat(respo? respo.toString() : 'fail respo')
     })();
