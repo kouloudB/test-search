71c2a7ca2a8188feadfb6230c78a3f99015befee
kouloudB <73066206+kouloudB@users.noreply.github.com>
2021-02-17 17:54:02 +0100

    Update Home.tsx

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -5,7 +5,9 @@ import { getAccounts } from '../actions/Accounts';
 import { onlineSearch } from '../actions/OnlineSearch';
 import { offlineSearch } from '../actions/OfflineSearch';
 import { ACCOUNT_SEARCH_ONLINE, ACCOUNT_SEARCH_OFFLINE, GET_ACCOUNTS} from '../types/Account'
-const url = "https://api.fda.gov/food/enforcement.json?limit=1000";
+const url = "https://api.fda.gov/food/enforcement.json?limit=50";
+const url2 = "https://motulvmo0ewtgpd9e.vms.hana.ondemand.com/dev/form/AccountSelection"
+
 function Home() {
   const accounts = useSelector((state: RootReducers) => state.accountsReducer.accounts)
   const isConnected = useSelector((state: RootReducers) => state.accountsReducer.isConnected)
@@ -16,34 +18,34 @@ function Home() {
   const handleAccountsSearch = async (event : React.ChangeEvent<HTMLInputElement>)=>{
     if(isConnected){
       const cache = await caches.open('accounts-cache');
-      cache.match(url)
+      cache.match(url2)
       .then(response=>{ 
         setConsCach(response==undefined? 'undefined': 'online data'+response?.toString())
          return response?.json()
       .then(({ results})=>{
          
       })})
-      dispatch(onlineSearch(
-        url, 
-        event.target.value, 
-        ACCOUNT_SEARCH_ONLINE,
-        ))
+      // dispatch(onlineSearch(
+      //   url, 
+      //   event.target.value, 
+      //   ACCOUNT_SEARCH_ONLINE,
+      //   ))
       console.log('>>> from API')
     }else{
       const cache = await caches.open('accounts-cache');
-      cache.match(url)
+      cache.match(url2)
       .then(response=>{ 
         setConsCach(response==undefined? 'undefined': 'offline data'+response?.toString())
          return response?.json()
       .then(({ results})=>{
          
       })})
-      dispatch(offlineSearch(
-        event.target.value, 
-        'accounts-cache', 
-        url,
-        ACCOUNT_SEARCH_OFFLINE
-        ))
+      // dispatch(offlineSearch(
+      //   event.target.value, 
+      //   'accounts-cache', 
+      //   url,
+      //   ACCOUNT_SEARCH_OFFLINE
+      //   ))
       console.log('>>> from CACHE')
     }
        
@@ -58,11 +60,11 @@ function Home() {
       <p>{isConnected? "online": "offline"}</p>
       <p>{"consCach  "+consCach}</p>
       <input type="text" onChange={handleAccountsSearch} />
-        <ul>
+        {/* <ul>
           {accounts.map(({city}, i)=> 
             <li key={i}>{city}</li>
           )}
-        </ul>
+        </ul> */}
     </div>
   );
 }
